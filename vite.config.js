937f02b04194a6b900284f86afff97e6dfa96866
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-05-18 22:10:39 +0500

    Loyihaning birinchi versiyasi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/react-1-lesson/', // bu yerda repo nomi bo'lishi shart!
   plugins: [react()],
 })
